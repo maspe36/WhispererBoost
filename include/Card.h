e2118750e0ee492ffe5634e34e9de159dd7f4a37
maspe36 <privett.sam@gmail.com>
2017-02-07 20:06:12 -0500

    Fleshed out the Card class some more. Added most instance vars needed to define an individual card

--- include/Card.h
+++ include/Card.h
@@ -6,8 +6,15 @@ using namespace std;
 class Card
 {
 public:
-	// The mana cost to play this card, 
-	// same order as the Color enum
+	#pragma region Instance Vars
+
+	/* The mana cost to play this card (Alphabetical)
+	0: Black
+	1: Blue
+	2: Brown
+	3: Green
+	4: Red
+	5: White */
 	vector<int> Cost;
 
 	// A cards name
@@ -17,21 +24,59 @@ public:
 	string Text;
 
 	// A cards color
-	enum _Color 
+	enum class ColorVariants
 	{
-		_Black,
-		_Blue,
-		_Brown,
-		_Green,
-		_Red,
-		_White
+		Black,
+		Blue,
+		Brown,
+		Green,
+		Red,
+		White
 	} Color;
 
 	// The card type
-	enum _Type 
+	enum class TypeVariants
 	{
-		_Constant,
-		_Creature,
-		_Spell
+		Constant,
+		Creature,
+		Spell
 	} Type;
+
+	// Different possible "mechanics" or types of effects
+	enum MechanicVariants
+	{
+		None,
+		Buff,
+		Debuff,
+		Continuous,
+		Downfall,
+		InHand,
+		Introduce,
+		OnDiscard,
+		OnDraw,
+		Rush,
+		Statis,
+		Trigger,
+		TurnStart,
+		TurnEnd
+	};
+
+	vector<MechanicVariants> Mechanics;
+
+	bool Alive;
+
+	#pragma endregion
+
+	#pragma region Virtual Methods
+
+	virtual void Effect();
+
+	#pragma endregion
+
+	#pragma region Constructor & Destructor
+
+	Card(vector<int> Cost, string Name, string Text, ColorVariants Color, TypeVariants Type, vector<MechanicVariants> Mechanics);
+	virtual ~Card();
+
+	#pragma endregion
 };
---
+++ src/Card.cpp
@@ -0,0 +1,15 @@
+#include "..\include\Card.h"
+
+void Card::Effect()
+{
+}
+
+Card::Card(vector<int> cost, string name, string text, ColorVariants color, TypeVariants type, vector<MechanicVariants> mechanics)
+	: Cost(cost), Name(name), Text(text), Color(color), Type(type), Mechanics(mechanics)
+{
+	Alive = true;
+}
+
+Card::~Card()
+{
+}
