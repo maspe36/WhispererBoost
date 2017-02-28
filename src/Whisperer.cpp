2a9616c7fb680ed664d49badfb1522d985af9c7d
maspe36 <privett.sam@gmail.com>
2017-02-27 20:52:33 -0500

    Added template code to the main function to call the new server class. It DOES use exceptions, as far as I know that is a boost feature of io_service

--- src/Whisperer.cpp
+++ src/Whisperer.cpp
@@ -1,6 +1,43 @@
 #include "stdafx.h"
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include "Network\Server.h"
+#include "boost\thread.hpp"
+
+using namespace std;
 
 int main()
 {
+	try
+	{
+		std::cout << "Initializing io_service..." << std::endl;
+		boost::asio::io_service io_service;
+		std::cout << "Starting server..." << std::endl;
+		Server server(io_service, 8888);
+		std::cout << "Server started!" << std::endl;
+
+		// Run the blocking io_service method run()
+		boost::thread ioThread(
+			boost::bind(&boost::asio::io_service::run, &io_service));
+
+		// Game logic and matchmaking logic goes here
+
+		/*
+		std::string data;
+		while (data != "exit")
+		{
+			std::getline(std::cin, data);
+			server.WriteToAll(data);
+		}
+		*/
+
+		ioThread.join();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
     return 0;
 }
