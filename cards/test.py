5cb6d82687f48a59581dbc47a5b730de0f6f8ea9
maspe36 <privett.sam@gmail.com>
2017-08-05 20:08:48 -0400

    Added the jamroot and a test python script for help when building

---
+++ cards/test.py
@@ -0,0 +1,20 @@
+import Card
+import Player
+import Game
+import Action
+import Client
+
+print(dir(Card))
+print(dir(Card.Card))
+
+print(dir(Player))
+print(dir(Player.Player))
+
+print(dir(Game))
+print(dir(Game.Game))
+
+print(dir(Action))
+print(dir(Action.Action))
+
+print(dir(Client))
+print(dir(Client.Client))
