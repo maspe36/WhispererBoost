ea15909ff36d482f439dd2070f96e810b899f021
maspe36 <privett.sam@gmail.com>
2017-08-12 10:14:27 -0400

    Updated the test.py to verify that all our modules have been built and are accessible.

--- cards/test.py
+++ cards/test.py
@@ -1,20 +1,32 @@
-import Card
-import Player
-import Game
-import Action
-import Client
-
-print(dir(Card))
-print(dir(Card.Card))
-
-print(dir(Player))
-print(dir(Player.Player))
-
-print(dir(Game))
-print(dir(Game.Game))
+import GameLibrary.Action as Action
+import GameLibrary.Card as Card
+import GameLibrary.Client as Client
+import GameLibrary.Constant as Constant
+import GameLibrary.Creature as Creature
+import GameLibrary.Game as Game
+import GameLibrary.Player as Player
+import GameLibrary.Spell as Spell
 
 print(dir(Action))
 print(dir(Action.Action))
 
+print(dir(Card))
+print(dir(Card.Card))
+
 print(dir(Client))
 print(dir(Client.Client))
+
+print(dir(Constant))
+print(dir(Constant.Constant))
+
+print(dir(Creature))
+print(dir(Creature.Creature))
+
+print(dir(Game))
+print(dir(Game.Game))
+
+print(dir(Player))
+print(dir(Player.Player))
+
+print(dir(Spell))
+print(dir(Spell.Spell))
